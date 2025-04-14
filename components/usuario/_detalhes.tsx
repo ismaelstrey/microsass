949418e75a4481791352726009c0946eb193b5c0
ismael strey pereira <ismaelstrey@hotmail.com>
2025-04-14 17:40:03 -0300

    refactor(usuario): adjust layout and add toggle for user details
    - Change the positioning of the user component from centered to fixed top-right
    - Add a toggle feature to show/hide user details with a clickable arrow icon

--- components/usuario/_detalhes.tsx
+++ components/usuario/_detalhes.tsx
@@ -1,11 +1,26 @@
 'use client'
+
+import { useState } from "react"
+import { FaArrowCircleDown } from "react-icons/fa"
+
 export default function UsuarioDetalhes({ name, email, emailVerified}:{name?:string,email?:string,emailVerified?:boolean}) {
+    const[ show, setShow ] = useState(false)
+    const handleClick = () => {
+        setShow(!show)
+    }
   return (
-    <div>        
-          <h1 className="text-4xl font-bold">{name}</h1>
+    <div> 
+        <FaArrowCircleDown onClick={handleClick} size={16}/>       
+{ show &&        
+
+<div>
+       <h1 className="text-4xl font-bold">{name}</h1>
           <p>{email}</p>      
           {emailVerified ? <p>Email verificado: {emailVerified}</p> : <p>Email não verificado</p>}
-        </div>  
+       </div>
+       }
+
+ </div>  
   )
 
 }
